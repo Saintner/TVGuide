8be3db8c34b554ee40823a2ea4bbe0082b5127f3
Eris Ramirez <kowayasky@gmail.com>
2022-06-18 02:29:01 -0500

    add TVGShowListEntity properties

---
+++ TVGuide/TVGuide/Common/Model/TVGGenreEntity.swift
@@ -0,0 +1,38 @@
+//
+//  TVGGenreEntity.swift
+//  TVGuide
+//
+//  Created by User on 18/06/22.
+//
+
+import Foundation
+enum TVGGenre: String, Codable {
+    case action = "Action"
+    case adult = "Adult"
+    case adventure = "Adventure"
+    case anime = "Anime"
+    case children = "Children"
+    case comedy = "Comedy"
+    case crime = "Crime"
+    case diy = "DIY"
+    case drama = "Drama"
+    case espionage = "Espionage"
+    case family = "Family"
+    case fantasy = "Fantasy"
+    case food = "Food"
+    case history = "History"
+    case horror = "Horror"
+    case legal = "Legal"
+    case medical = "Medical"
+    case music = "Music"
+    case mistery = "Mystery"
+    case nature = "Nature"
+    case romance = "Romance"
+    case scienceFiction = "Science-Fiction"
+    case sports = "Sports"
+    case supernatural = "Supernatural"
+    case thriller = "Thriller"
+    case travel = "Travel"
+    case war = "War"
+    case western = "Western"
+}
---
+++ TVGuide/TVGuide/Common/Model/TVGShowEntity.swift
@@ -0,0 +1,76 @@
+//
+//  TVGShowsEntity.swift
+//  TVGuide
+//
+//  Created by User on 18/06/22.
+//
+
+import Foundation
+
+struct TVGShowEntity: TVGEntity {
+    var id: Int
+    var url: String
+    var name: String
+    var type: String
+    var language: String
+    var genre: [TVGGenre]
+    var status: String
+    var runtime: Int
+    var averageRuntime: Int
+    var premiered: String
+    var ended: String
+    var officialSite: String
+    var schedule: Schedule
+    var rating: Rating
+    var weight: Int
+    var network: Network
+    var webChannel: String?
+    var dvdCountry: String?
+    var externals: Externals
+    var image: Image
+    var summary: String
+    var updated: Int
+    var _links: Links
+    struct Schedule: TVGEntity {
+        var time: String
+        var days: [String]
+    }
+    
+    struct Rating: TVGEntity {
+        var average: Double
+    }
+    
+    struct Network : TVGEntity {
+        var id: Int
+        var name: String
+        var country: TVGCountry
+        var officialSite: String
+        struct TVGCountry: TVGEntity {
+            var name: String
+            var code: String
+            var timezone: String
+        }
+    }
+    
+    struct Externals: TVGEntity {
+        var tvrage: Int
+        var thetvdb: Int
+        var imdb: Int
+    }
+    
+    struct Image: TVGEntity {
+        var medium: String
+        var original: String
+    }
+    
+    struct Links: TVGEntity {
+        var `self`: Self
+        var previousepisode: PreviousEpisode
+        struct `Self`: TVGEntity {
+            var href: String
+        }
+        struct PreviousEpisode: TVGEntity {
+            var href: String
+        }
+    }
+}
